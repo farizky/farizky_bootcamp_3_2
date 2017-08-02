bc2d882cb5507fcd422b68f0535ca99bb07d2844
Ryan Henry <ryan.henry20@hotmail.com>
2017-08-02 17:07:55 +0700

    commit1
    commit1

---
+++ bootcamp_backend/app/Http/Controllers/myController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller
+{
+    function getData
+{
+    //
+}
+
+function addData
+{
+    DB::beginTransaction();
+
+    try{
+        $this->validate($request, [])
+    }
+}
+
+function removeData
+{
+    //
+}
+
+}
